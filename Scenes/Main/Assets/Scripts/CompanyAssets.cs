7b416c287615fced4fb336585daf9a187406ce51
Eddy He <eddyheee@gmail.com>
2025-02-24 20:35:22 -0600

    refactor CompanyAssets code

--- Scenes/Main/Assets/Scripts/CompanyAssets.cs
+++ Scenes/Main/Assets/Scripts/CompanyAssets.cs
@@ -6,19 +6,42 @@ public partial class CompanyAssets : CanvasLayer
 	
 	private AnimationPlayer _animPlayer;
 	private Button _assetButton;
+	private Button _closeAssetButton;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Button closeAssetButton = GetNode<Button>("%CloseAsset");
+		// Get close asset button
+		GetCloseAsset();
+		// Get the asset button
+		GetAssetButton();
+		// Get the animation player
+		GetAnimationPlayer();
+	}
+
+	public void OncloseAssetButtonPressed(){
+		GD.Print("Close Asset button pressed...");
 		
-		if(closeAssetButton != null){
-			closeAssetButton.Pressed += OncloseAssetButtonPressed;
+		_animPlayer?.Play("TransOut");
+		
+		// Show and enable the button
+		if(_assetButton != null)	{
+			_assetButton.Visible = true; // Make visible
+			_assetButton.Disabled = false;  // allow interaction
+		}
+	}
+	
+	private void GetCloseAsset(){
+		_closeAssetButton = GetNode<Button>("%CloseAsset");
+		
+		if(_closeAssetButton != null){
+			_closeAssetButton.Pressed += OncloseAssetButtonPressed;
 		} else {
 			GD.PrintErr("closeAssetButton not found!");
 		}
-		
-		// Get the asset button
+	}
+	
+	private void GetAssetButton(){
 		_assetButton = GetNode<Button>($"../main_controls/AssetButton");
 		
 		if(_assetButton != null){
@@ -26,23 +49,15 @@ public partial class CompanyAssets : CanvasLayer
 		} else {
 			GD.PrintErr("assetButton not found!");
 		}
-		
-		// Get the animation player
+	}
+	
+	private void GetAnimationPlayer(){
 		_animPlayer = GetNode<AnimationPlayer>($"Anim");
 		if(_animPlayer == null)
 		{
 			GD.PrintErr("AnimationPlayer not found!");
 		}
 	}
-
-	public void OncloseAssetButtonPressed(){
-		GD.Print("Close Asset button pressed...");
-		
-		_animPlayer?.Play("TransOut");
-		// Show and enable the button
-		if(_assetButton != null)	{
-			_assetButton.Visible = true; // Make visible
-			_assetButton.Disabled = false;  // allow interaction
-		}
-	}
+	
+	
 }
