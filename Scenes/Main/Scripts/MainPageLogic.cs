07153f2b8ccac03be0ebd1b4625c4e1bd5f79cc6
Eddy He <eddyheee@gmail.com>
2025-02-24 20:38:10 -0600

    refactor MainPageLogic class

--- Scenes/Main/Scripts/MainPageLogic.cs
+++ Scenes/Main/Scripts/MainPageLogic.cs
@@ -9,20 +9,11 @@ public partial class MainPageLogic : ParallaxBackground
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_assetButton = GetNode<Button>("%AssetButton");
-		
-		if(_assetButton != null){
-			_assetButton.Pressed += OnAssetButtonPressed;
-		} else {
-			GD.PrintErr("assetButton not found!");
-		}
-		
+		// Get asset button
+		GetAssetButton();
 		// Get the animation player
-		_animPlayer = GetNode<AnimationPlayer>($"../company_assets/Anim");
-		if(_animPlayer == null)
-		{
-			GD.PrintErr("AnimationPlayer not found!");
-		}
+		GetAnimationPlayer();
+		
 	}
 	
 	public void OnAssetButtonPressed(){
@@ -34,6 +25,23 @@ public partial class MainPageLogic : ParallaxBackground
 			_assetButton.Visible = false; // Make invisible
 			_assetButton.Disabled = true;  // Prevent interaction
 		}
+	}
+	
+	private void GetAssetButton(){
+		_assetButton = GetNode<Button>("%AssetButton");
 		
+		if(_assetButton != null){
+			_assetButton.Pressed += OnAssetButtonPressed;
+		} else {
+			GD.PrintErr("assetButton not found!");
+		}
+	}
+	
+	private void GetAnimationPlayer(){
+		_animPlayer = GetNode<AnimationPlayer>($"../company_assets/Anim");
+		if(_animPlayer == null)
+		{
+			GD.PrintErr("AnimationPlayer not found!");
+		}
 	}
 }
